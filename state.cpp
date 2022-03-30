0ced34efb219c1d207a41f9f25b865bdd0f8cc76
Rushith Nalin Vincent Karunaratne <rukarunaratne@gmail.com>
2022-03-30 17:54:39 +1100

    Thread test

--- state.cpp
+++ state.cpp
@@ -1,14 +1,34 @@
 #include "Light.h" 
 #include "LightState.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+bool check = true;
+
+void thread_test(const char* name) {
+    unsigned int count = 0;
+    while (check) {
+        count++;
+    }
+    printf("%s count %u.\n", name, count);
+}
 
 int main() { 
     
-
+    std::thread t1(thread_test, "user1");
+    std::thread t2(thread_test, "user2");
+    
+	printf("Starting threads \n");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    check = false;
+    
     Light light; 
-    for (int i = 0; i < 1000000 ; i++){
+    for (int i = 0; i < 100 ; i++){
         light.toggle();
     }
+    t1.join();
+    t2.join();
     
     return 0; 
 } 
